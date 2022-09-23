e43b1da547480346e068271c0ff5fa24fa018e43
weddingcarride <weddingcarride@gmail.com>
2022-09-23 11:23:27 +0530

    added gallery

--- website/urls.py
+++ website/urls.py
@@ -9,6 +9,7 @@ from django.urls import re_path as url
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
+    path('gallery/', include('gallery.urls')),
 
     url(r'^static/(?P<path>.*)$', serve,
         {'document_root': settings.STATIC_ROOT})
